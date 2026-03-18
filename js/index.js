4ba58fda5808f4b988a2b1eb8141c8fcbc87a22d
Cleysson Santos <cleyssonsantospalmeira@gmail.com>
2026-03-18 20:19:20 -0300

    adicionando arquivo js e aumentando o tamanho do menu

---
+++ js/index.js
@@ -0,0 +1,12 @@
+(function(){
+const items = document.querySelectorAll('.menu-item');
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    
+    items.forEach(i => i.classList.remove('active'));
+    
+    item.classList.add('active');
+  });
+});
+})();

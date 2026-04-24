bc66554d7ba66bb8a04c5b03a07a420365a57395
Cleysson Santos <cleyssonsantospalmeira@gmail.com>
2026-04-24 20:31:14 -0300

    Adicionando setas e dots nas sections e funcionalidade no javascript

--- js/index.js
+++ js/index.js
@@ -1,6 +1,58 @@
 // marcador de link
 
 (function(){
+
+const positions = {};
+const dotsMap = {};
+
+function move (trackId, direction) {
+  const track = document.getElementById(trackId);
+  const total = track.children.length;
+  
+  if (!positions[trackId]) positions[trackId] = 0;
+
+  positions[trackId] += direction;
+
+  if (positions[trackId] < 0) positions[trackId] = 0;
+  if (positions[trackId] >= total) positions[trackId] = total - 1;
+
+  track.style.transform = `translateX(-${positions[trackId] * 100}%)`;
+
+  updateDots(trackId);
+}
+
+// SWIPE MOBILE
+
+function enableSwipe(trackId) {
+  const track = document.getElementById(trackId);
+  let startX = 0;
+  let endX = 0;
+
+  track.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+  });
+
+  track.addEventListener('touchmove', (e) => {
+    endX = e.touches[0].clientX;
+  });
+
+  track.addEventListener('touchend', () => {
+    const diff = startX - endX;
+
+    if (Math.abs(diff) > 50) {
+      if (diff > 0) {
+        move(trackId, 1);
+      } else {
+        move(trackId, -1);
+      }
+    }
+  });
+}
+
+// ATIVAR SWIPE NOS CARROSEIS
+enableSwipe('bannerTrack');
+enableSwipe('pedidoTrack');
+
 const items = document.querySelectorAll('.menu-item');
 
 items.forEach(item => {

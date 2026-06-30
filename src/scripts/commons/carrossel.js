832378c619cdcb0974902b2b9862d9c6d5b67587
Cleysson Santos <cleyssonsantospalmeira@gmail.com>
2026-06-30 19:39:37 -0300

    ajustando link de caminho das imagens e script

--- src/scripts/commons/carrossel.js
+++ src/scripts/commons/carrossel.js
@@ -1,105 +1,35 @@
-// // marcador de link
+(function(){
 
- (function(){
+    const bannerTrack = document.getElementById('bannerTrack');
+    const bannerItems = bannerTrack.querySelectorAll('.carousel-item');
+    const bannerPrev = document.querySelector('.heroBanner .arrow.left');
+    const bannerNext = document.querySelector('.heroBanner .arrow.right');
 
-const input = document.getElementById('searchForm');
-const lupa = document.getElementById('btn-search');
-const menu = document.getElementById('btn-menu');
-const searchInputField = document.getElementById('search-input');
-const btnMenu = document.getElementById('btn-menu');
-const menuModal = document.getElementById('menuModal');
-const closeMenu = document.getElementById('closeMenu');
-const currentPage = window.location.pathname.split("/").pop().replace('.html', '');
+    let bannerIndex = 0;
 
-lupa.addEventListener('click', (event) => {
-    event.stopPropagation();
-    input.classList.add('active');
-    lupa.classList.add('hidden');
-
-    setTimeout(() => {
-        searchInputField.focus();
-    }, 50);
-});
-
-function closeSearch() {
-    console.log('Fechando busca')
-    input.classList.remove('active');
-    lupa.classList.remove('hidden');
-}
-
-document.addEventListener('click', (event) => {
-    const clicouFora = !input.contains(event.target) && !lupa.contains(event.target);
-    
-    if (input.classList.contains('active') && clicouFora && searchInputField.value.trim() === '') {
-        closeSearch();
-    }
-});
-
-document.addEventListener('keydown', (event) => {
-    if(event.key === 'Escape' && input.classList.contains('active')) {
-        closeSearch();
-    }
-});
-
-btnMenu.addEventListener('click', () => {
-    menuModal.classList.add('active');
-    btnMenu.style.display = 'none';
-});
-
-closeMenu.addEventListener('click', () => {
-    menuModal.classList.remove('active');
-    btnMenu.style.display = 'block';
-});
-
-menuModal.addEventListener('click', (e) => {
-    if (e.target === menuModal) {
-        menuModal.classList.remove('active');
-    }
-});
-
-if (currentPage === '') {
-    currentPage = 'index';
-}
-
-document.querySelectorAll('.menu-content a').forEach(link => {
-
-    if(link.dataset.page === currentPage) {
-        link.classList.add('active');
-    }
-});
-
-// HEROBANNER
-
-const bannerTrack = document.getElementById('bannerTrack');
-const bannerItems = bannerTrack.querySelectorAll('.carousel-item');
-const bannerPrev = document.querySelector('.heroBanner .arrow.left');
-const bannerNext = document.querySelector('.heroBanner .arrow.right');
-
-let bannerIndex = 0;
-
-function updateBanner() {
-    bannerTrack.style.transform =
-        `translateX(-${bannerIndex * 100}%)`;
-}
-
-bannerNext.addEventListener('click', () => {
-    bannerIndex++;
-
-    if(bannerIndex >= bannerItems.length) {
-        bannerIndex = 0;
+    function updateBanner() {
+        bannerTrack.style.transform =
+            `translateX(-${bannerIndex * 100}%)`;
     }
 
-    updateBanner();
-});
+    bannerNext.addEventListener('click', () => {
+        bannerIndex++;
 
-bannerPrev.addEventListener('click', () => {
-    bannerIndex--;
+        if(bannerIndex >= bannerItems.length) {
+            bannerIndex = 0;
+        }
 
-    if(bannerIndex < 0) {
-        bannerIndex = bannerItems.length - 1;
-    }
+        updateBanner();
+    });
 
-    updateBanner();
+    bannerPrev.addEventListener('click', () => {
+        bannerIndex--;
+
+        if(bannerIndex < 0) {
+            bannerIndex = bannerItems.length - 1;
+        }
+
+        updateBanner();
 });
 
 // SWIPE
---
+++ src/scripts/commons/header.js
@@ -0,0 +1,68 @@
+(function () {
+    const input = document.getElementById('searchForm');
+const lupa = document.getElementById('btn-search');
+const menu = document.getElementById('btn-menu');
+const searchInputField = document.getElementById('search-input');
+const btnMenu = document.getElementById('btn-menu');
+const menuModal = document.getElementById('menuModal');
+const closeMenu = document.getElementById('closeMenu');
+const currentPage = window.location.pathname.split("/").pop().replace('.html', '');
+
+lupa.addEventListener('click', (event) => {
+    event.stopPropagation();
+    input.classList.add('active');
+    lupa.classList.add('hidden');
+
+    setTimeout(() => {
+        searchInputField.focus();
+    }, 50);
+});
+
+function closeSearch() {
+    console.log('Fechando busca')
+    input.classList.remove('active');
+    lupa.classList.remove('hidden');
+}
+
+document.addEventListener('click', (event) => {
+    const clicouFora = !input.contains(event.target) && !lupa.contains(event.target);
+    
+    if (input.classList.contains('active') && clicouFora && searchInputField.value.trim() === '') {
+        closeSearch();
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'Escape' && input.classList.contains('active')) {
+        closeSearch();
+    }
+});
+
+btnMenu.addEventListener('click', () => {
+    menuModal.classList.add('active');
+    btnMenu.style.display = 'none';
+});
+
+closeMenu.addEventListener('click', () => {
+    menuModal.classList.remove('active');
+    btnMenu.style.display = 'block';
+});
+
+menuModal.addEventListener('click', (e) => {
+    if (e.target === menuModal) {
+        menuModal.classList.remove('active');
+    }
+});
+
+if (currentPage === '') {
+    currentPage = 'index';
+}
+
+document.querySelectorAll('.menu-content a').forEach(link => {
+
+    if(link.dataset.page === currentPage) {
+        link.classList.add('active');
+    }
+});
+
+})();
